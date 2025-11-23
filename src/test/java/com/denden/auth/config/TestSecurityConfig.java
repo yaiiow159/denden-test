25c6e868374bae34f7ccf134935c3ed884bbc70f
Timmy <examyou076@gmail.com>
2025-11-23 10:47:25 +0800

    [Bug] - fix duplicate key for spring config yml

--- src/test/java/com/denden/auth/config/TestSecurityConfig.java
+++ src/test/java/com/denden/auth/config/TestSecurityConfig.java
@@ -4,10 +4,10 @@ import com.denden.auth.filter.JwtAuthenticationFilter;
 import com.denden.auth.filter.LoggingFilter;
 import com.denden.auth.filter.RateLimitFilter;
 import com.denden.auth.service.TokenService;
+import com.denden.auth.service.email.EmailSender;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @TestConfiguration
 public class TestSecurityConfig {
@@ -23,4 +23,10 @@ public class TestSecurityConfig {
 
     @MockBean
     private RateLimitFilter rateLimitFilter;
+    
+    @MockBean
+    private EmailSender emailSender;
+    
+    @MockBean
+    private RedisTemplate<String, Object> redisTemplate;
 }
